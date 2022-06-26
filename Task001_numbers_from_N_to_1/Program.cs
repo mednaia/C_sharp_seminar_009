f28cd80a07f1bbc15b307a9ca12f86430622caef
Ekaterina <tiurina.kate@gmail.com>
2022-06-26 15:50:53 +0300

    Task001 numbers from N to 1

---
+++ Task001_numbers_from_N_to_1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Показать натуральные числа от N до 1, N задано
+
+// for(int n = 10; n>0; n--)
+// {
+//     Console.Write($"{n} ");
+// }
+
+// string NumbersRec(int a, int n)
+// {
+// if (a <= n) return NumbersRec(a + 1, n) + $"{a} ";
+// else return String.Empty;
+// }
+// Console.WriteLine(NumbersRec(1, 10));
+
+void Num(int n)
+{
+    if(n > 0)
+    {
+        Console.Write("{0} ", n);
+        Num(n - 1);
+    }
+}
+
+Console.WriteLine("Input the number");
+int n = int.Parse(Console.ReadLine());
+Num(n);
+
+
+
