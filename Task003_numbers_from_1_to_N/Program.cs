b95d541b68b34f6180203c1e76a637c2af58c02d
Ekaterina <tiurina.kate@gmail.com>
2022-06-26 18:35:30 +0300

    Task 003 numbers from 1 to N

---
+++ Task003_numbers_from_1_to_N/Program.cs
@@ -0,0 +1,24 @@
+﻿// Показать натуральные числа от 1 до N, N задано
+///Вариант 1
+// void NumberFor(int n)
+// {
+//     for(int i = 1; i<=n; i++) Console.Write($"{i} ");
+// }
+// NumberFor(10);
+
+///Вариант 2
+// string NumbersRec(int a, int b)
+// {
+// if (a <= b) return $"{a} " + NumbersRec(a + 1, b);
+// else return String.Empty;
+// }
+// Console.WriteLine(NumbersRec(1, 10));
+
+///Вариант 3
+void PrintNumber(int n)
+{
+    if(n < 1) return;
+    PrintNumber(n-1);
+    Console.Write(n + " ");
+}
+PrintNumber(10);
