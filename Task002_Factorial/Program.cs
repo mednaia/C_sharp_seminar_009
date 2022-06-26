9a4154b7fbc91c2a0fb6631d212a38881813acfd
Ekaterina <tiurina.kate@gmail.com>
2022-06-26 16:24:21 +0300

    Task002 Factorial

---
+++ Task002_Factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿// Факториал числа
+
+// int Factorial(int n)
+// {
+//     int factorial = 1;
+//     for(;n>0; n--)
+//     {
+//         factorial = factorial*n;
+//     }
+//     return factorial;
+// }
+// Console.Write(Factorial(10));
+
+
+int FactorialRec(int n)
+{
+if (n == 1) return 1;
+else return n * FactorialRec(n - 1);
+}
+Console.WriteLine(FactorialRec(10)); 
+
